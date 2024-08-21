fed2941018697196e3e4d8beaebe5970c3241f94
chaos086 <luisa.marinh@uqvirtual.edu.co>
2024-08-20 19:44:42 -0500

    Se corrigue nombre del main que no debaja ejecutar

--- src/main/java/co/edu/uniquindio/gominola/view/MainView.java
+++ src/main/java/co/edu/uniquindio/gominola/view/MainView.java
@@ -4,7 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 
-public class giMainView {
+public class MainView {
     @FXML
     private ResourceBundle resources;
 
