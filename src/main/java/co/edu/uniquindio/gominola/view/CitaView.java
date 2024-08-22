f6fa336ba16e2bcce495ff07af1370bacffccac6
Carlos Colorado <cscalomejor214@gmail.com>
2024-08-22 17:04:23 -0500

    Boceto interfaz de la cita
    no tenia claro como hacer la interfaz de la cita, por eso en la U me cuentan si le cambiamos algo, y centré la interfaz de cliente, aunque en pantalla completa aun no se acomodan :(

--- src/main/java/co/edu/uniquindio/gominola/view/CitaView.java
+++ src/main/java/co/edu/uniquindio/gominola/view/CitaView.java
@@ -1,4 +1,110 @@
 package co.edu.uniquindio.gominola.view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 public class CitaView {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnAgregarCita;
+
+    @FXML
+    private Button btnEliminarCita;
+
+    @FXML
+    private GridPane gpDatosAcompañante;
+
+    @FXML
+    private Label lbFechaCita;
+
+    @FXML
+    private Label lbHoraCita;
+
+    @FXML
+    private Label lbHorasSolicitadas;
+
+    @FXML
+    private Label lbLugarEncuentro;
+
+    @FXML
+    private Label lbNombreAcompañanteCita;
+
+    @FXML
+    private Label lbNombreClienteCita;
+
+    @FXML
+    private TableColumn<?, ?> tblCorreoAcompañante;
+
+    @FXML
+    private TableColumn<?, ?> tblDisponibilidadAcompañante;
+
+    @FXML
+    private TableColumn<?, ?> tblEdadAcompañante;
+
+    @FXML
+    private TableColumn<?, ?> tblNombreAcompañante;
+
+    @FXML
+    private TableColumn<?, ?> tblSaludoAcompañante;
+
+    @FXML
+    private TableColumn<?, ?> tblTallaAcompañante;
+
+    @FXML
+    private TableColumn<?, ?> tblTelefonoAcompañante;
+
+    @FXML
+    private TableColumn<?, ?> tblValorHoraAcompañante;
+
+    @FXML
+    private TableView<?> tvTablaCitas;
+
+    @FXML
+    private TextField txtCantidadHorasCita;
+
+    @FXML
+    private TextField txtFechaCita;
+
+    @FXML
+    private TextField txtHoraCita;
+
+    @FXML
+    private TextField txtLugarCita;
+
+    @FXML
+    private TextField txtNombreAcompañanteCita;
+
+    @FXML
+    private TextField txtNombreClienteCita;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    @FXML
+    void agregarCitaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarCitaAction(ActionEvent event) {
+
+    }
+
+
 }
--- src/main/java/co/edu/uniquindio/gominola/view/ClienteView.java
+++ src/main/java/co/edu/uniquindio/gominola/view/ClienteView.java
@@ -68,6 +68,7 @@ public class ClienteView {
     void initialize() {
 
 
+
     }
 
 }
