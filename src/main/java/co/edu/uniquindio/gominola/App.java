62cd3c15826cebe386b16aa6c3ff553f354e4e5e
Carlos Colorado <cscalomejor214@gmail.com>
2024-08-25 14:49:09 -0500

    ajustes interfaz

--- src/main/java/co/edu/uniquindio/gominola/App.java
+++ src/main/java/co/edu/uniquindio/gominola/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 767, 520);
+        Scene scene = new Scene(fxmlLoader.load(), 750, 520);
         stage.setTitle("Gominola: acompaña tu vida");
         stage.setScene(scene);
         stage.show();
