34f4b24ea9b0d7f918bd759db3ed4d553da36a35
chaos086 <luisa.marinh@uqvirtual.edu.co>
2024-08-25 00:28:58 -0500

    Se crearon Crud de clienteController

--- src/main/java/co/edu/uniquindio/gominola/controller/ClienteController.java
+++ src/main/java/co/edu/uniquindio/gominola/controller/ClienteController.java
@@ -1,8 +1,14 @@
 package co.edu.uniquindio.gominola.controller;
 
 import co.edu.uniquindio.gominola.factory.ModelFactory;
+import co.edu.uniquindio.gominola.model.Acompanante;
+import co.edu.uniquindio.gominola.model.Cliente;
 import co.edu.uniquindio.gominola.model.Cliente;
 import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Objects;
 
 public class ClienteController {
     private ModelFactory factory;
@@ -21,4 +27,36 @@ public class ClienteController {
         this.listaClienteObservable.addAll(this.factory.getGominola().getListaCliente());
     }
 
+    public void eliminarCliente(String nombre){
+        ArrayList<Cliente> Cliente = factory.getGominola().getListaCliente();
+        for (int i = 0; i < Cliente.size(); i++){
+            if (Objects.equals(Cliente.get(i).getNombre(), nombre)){
+                Cliente.remove(Cliente.get(i));
+                this.listaClienteObservable.remove(Cliente.get(i));
+            }
+        }
+    }
+
+    public Cliente consultarCliente(String nombre){
+        ArrayList<Cliente> Cliente = this.factory.getGominola().getListaCliente();
+        for (Cliente value : Cliente) {
+            if (value.getNombre().equals(nombre)){
+                return value;
+            }
+        }
+        return null;
+    }
+
+    public String crearCliente(String nombre, String correo, String telefono) {
+        ArrayList<Cliente> Clientes = factory.getGominola().getListaCliente();
+        for (Cliente Cliente : Clientes) {
+            if (Objects.equals(Cliente.getNombre(), nombre)) {
+                return "El Clientes ingresado ya se existe";
+            }
+        }
+        Cliente nuevoCliente = new Cliente(nombre, correo, telefono);
+        this.factory.getGominola().addCliente(nuevoCliente);
+        this.listaClienteObservable.add(nuevoCliente);
+        return "Cliente registrado exitosamente";
+    }
 }
--- src/main/java/co/edu/uniquindio/gominola/model/Cliente.java
+++ src/main/java/co/edu/uniquindio/gominola/model/Cliente.java
@@ -34,4 +34,6 @@ public class Cliente {
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+
+
 }
